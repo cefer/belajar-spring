37e968649aed299207f438446489241b0f4811d8
syarip.hidayat <hidayatsyarip@gmail.com>
2016-10-07 17:00:44 +0700

    Initial Commit

---
+++ src/main/java/com/dlabs/belajar/spring/App.java
@@ -0,0 +1,13 @@
+package com.dlabs.belajar.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
